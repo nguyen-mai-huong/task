7d68628cc1c7b40f5114226a0f8946f302fee45c
maihuong.nguyen <nguyenmaihuong9009@gmail.com>
2021-01-19 20:46:18 +0800

    1. add Cors config to allow GET, POST requests from React frontend app 2. provide authentication manager using UserDetailsService and password encryption.

--- src/main/java/com/ufinity/task/WebSecurityConfig.java
+++ src/main/java/com/ufinity/task/WebSecurityConfig.java
@@ -1,11 +1,20 @@
 package com.ufinity.task;
 
+import com.ufinity.task.security.UserAuthenticationHelperService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -17,15 +26,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     return bCryptPasswordEncoder;
   }
 
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
+  @Autowired
+  private BCryptPasswordEncoder passwordEncoder;
+
+  @Autowired
+  private UserAuthenticationHelperService helperService;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.cors();
+
     http.authorizeRequests()
             .antMatchers("/").permitAll()
             .antMatchers("/user/login").permitAll()
-            .antMatchers("/users/list").permitAll()
             .antMatchers("/user/signup").permitAll()
-            .antMatchers("/user/roles").permitAll();
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginProcessingUrl("/user/login").usernameParameter("username").passwordParameter("password");
 
     http.csrf().ignoringAntMatchers("/user/login").ignoringAntMatchers("/user/signup");
+
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(helperService).passwordEncoder(passwordEncoder);
   }
 }
