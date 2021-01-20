71252eaed5e96c40767eb1a6ff7057b1f28459b1
maihuong.nguyen <nguyenmaihuong9009@gmail.com>
2021-01-20 15:01:21 +0800

    add global CORS config for api services

--- src/main/java/com/ufinity/task/TaskApplication.java
+++ src/main/java/com/ufinity/task/TaskApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -27,5 +29,20 @@ public class TaskApplication {
 
 		};
 	} */
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedMethods = new String[] {"GET", "POST"};
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(allowedMethods);
+      }
+    };
+
+  }
 
 }
